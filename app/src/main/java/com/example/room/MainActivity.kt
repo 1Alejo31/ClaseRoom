ac87b2081f45b7d8c37140b0afceb97121f9525d
Krhistian Alejandro Gonzalez Duarte <krhistian.gonzalez@vasslatam.com>
2022-08-14 18:58:30 -0500

    Instanciando los objetos

--- app/src/main/java/com/example/room/MainActivity.kt
+++ app/src/main/java/com/example/room/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Instanciando los objetos
+
+        val nombre = findViewById<EditText>(R.id.etNombre)
+        val usuario = findViewById<EditText>(R.id.etUsuario)
+        val consulta = findViewById<EditText>(R.id.etConsulta)
+        val pasword = findViewById<EditText>(R.id.etPassword)
+
+        //  Instanciando los botones
+        val mostrar = findViewById<Button>(R.id.btnMostrar)
+        val guardar = findViewById<Button>(R.id.btnGuardar)
+
     }
 }
